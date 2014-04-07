e26c8e4bb85f901577bb076116244ac373e3403b
brayden2544 <brayden2544@gmail.com>
2014-04-07 10:26:31 -0600

    Initial Commit

---
+++ MyStuff/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyStuff
+//
+//  Created by Brayden Adams on 4/7/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyStuff/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyStuff
+//
+//  Created by Brayden Adams on 4/7/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
